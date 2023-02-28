a8ec82211e844a37455a5e1d02d844ac7a1099bb
Asef Jamil Ajwad <sigma.asef@gmail.com>
2023-03-01 03:00:51 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Warning
+
+## This code currently has no fail-safe. It takes complete control over all mouse movement as long as the colour #00FF00 is on screen.
---
+++ aimtbooster.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import time
+import random
+import mss
+import mouse
+
+time.sleep(2)
+
+X_OFFSET = 585
+Y_OFFSET = 440
+
+WIDTH = 750
+HEIGHT = 525
+
+sct = mss.mss()
+mon = {"top": Y_OFFSET, "left": X_OFFSET, "width": WIDTH, "height": HEIGHT}
+
+while True:
+
+    #last_time = time.time()
+
+    printscreen_np = np.asarray(sct.grab(mon))
+
+    loc = np.argwhere(np.logical_and(printscreen_np[:,:,0] == 0,
+                                     printscreen_np[:,:,1] == 255,
+                                     printscreen_np[:,:,2] == 0))
+
+    if len(loc) > 1: 
+        target = random.randrange(len(loc))
+        mouse.move(X_OFFSET + loc[target][1], Y_OFFSET + loc[target][0], absolute=True)
+        
+        mouse.click()
+
+
+    #cv2.imshow("", printscreen_np[:,:,::])
+    #print(f"{(time.time() - last_time) * 1000} ms")
+
+    #if cv2.waitKey(25) & 0xFF == ord('q'):
+        #cv2.destroyAllWindows()
+        #break
